5fd89ec1ee1b6477b123045cdd0006c22bb7fec2
Vlajkousa <vargirovic@yahoo.com>
2021-05-22 14:11:14 -0400

    added new line

--- src/work_1/Hello.java
+++ src/work_1/Hello.java
@@ -3,5 +3,6 @@ package work_1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work started");
+        System.out.println("Developing hello feature ");
     }
 }
