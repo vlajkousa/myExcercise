fcd297b889becc80a2291205a5ac7d733929d204
Vlajkousa <vargirovic@yahoo.com>
2021-05-22 14:11:59 -0400

    Having fun

--- src/work_1/Hello.java
+++ src/work_1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work started");
         System.out.println("Developing hello feature ");
+        System.out.println("Doing more work");
     }
 }
